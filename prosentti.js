ccaa874c72286ccdba474484eacb4c8402dd0eeb
jamaik123 <jussimakiikola@gmail.com>
2017-04-02 01:04:39 +0300

    added colour

--- prosentti.js
+++ prosentti.js
@@ -1,14 +1,18 @@
 var number = 50;
 var colour =71-(number-5)*71/90;
 var numberLabel = document.getElementById("number");
+var colourBack = document.getElementById("leftTop");
 var seconds=0;
 
 setInterval(setColour,1000);
 
 function setColour(){
 	if (seconds%10===0 ){
+		updatePercentage();
+		colour =71-(number-5)*71/90;
+		var hslColour = "hsl("+colour+",100%,30%)";
 		numberLabel.innerHTML=number+"%";
-		
+		colourBack.style.backgroundColor=hslColour;
 	}
 	seconds=seconds+1;
 }
