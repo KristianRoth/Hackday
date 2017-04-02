f899dc11eee745f189b1b6cecb6cc3fcc3306101
JJKETO <jjketo@utu.fi>
2017-04-02 10:05:44 +0300

    Ennusteen varianssia korjattu

--- Ennuste/Ennuste.R
+++ Ennuste/Ennuste.R
@@ -2,7 +2,7 @@
 fileConn<-file("values.txt")
 pred <- readLines(fileConn)
 pred <- list(ennuste = as.numeric(pred[1]), muutos = as.numeric(pred[2]))
-muutos <- rnorm(1,pred$muutos/2,0.01)
+muutos <- rnorm(1,pred$muutos/2,0.03)
 ennuste <- max(min(pred$ennuste+muutos,0.95),0.05)
 writeLines(c(as.character(ennuste),as.character(muutos)), fileConn)
 cat(round(ennuste,2)*100)
